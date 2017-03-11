8f77791adc304678ae8b74e11603dee71272273e
Eduardo Vargas <eduardo.vargas@hipteam.io>
2017-03-11 11:24:50 +0100

    'new'

--- server.js
+++ server.js
@@ -13,10 +13,14 @@ const port = 8001;
 app.listen(port, () => {
     console.log('We are live on ' + port);
 });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.get('/', (req, resp) => {
 
 })
-
 
 app.get('/cure', (req, resp) => {
     var parser = new xml2js.Parser();
